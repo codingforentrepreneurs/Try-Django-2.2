748d52618acd0321c4ccdd73caaf76f20a69ca37
CFE <hello@teamcfe.com>
2019-04-02 11:51:54 -0700

    15 - Template Context Processors

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -6,12 +6,7 @@ from django.template.loader import get_template
 def home_page(request):
     my_title = "Hello there...."
     context = {"title": my_title}
-    template_name   = "title.txt" 
-    template_obj    = get_template(template_name)
-    rendered_string  = template_obj.render(context)
-    # doc = "<h1>{title}</h1>".format(title=my_title)
-    # django_rendered_doc = "<h1>{{title}}</h1>".format(title=my_title)
-    return render(request, "hello_world.html", {"title": rendered_string})
+    return render(request, "home.html", context)
 
 
 def about_page(request):
