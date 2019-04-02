c34a1d1b8274d6a7939b2ae4543a2c3b8408fd42
CFE <hello@teamcfe.com>
2019-04-02 10:54:06 -0700

    9 - Your First Template

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "hello_world.html")
 
 
 def about_page(request):
