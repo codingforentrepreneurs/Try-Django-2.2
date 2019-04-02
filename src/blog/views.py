3c3aef27f2ababa85d8527670bfd8f0806dceaf3
CFE <hello@teamcfe.com>
2019-04-02 16:20:29 -0700

    30 - Routing the Views

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,23 +4,6 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import BlogPost
 
-
-# GET -> 1 object
-# filter -> [] objects
-
-def blog_post_detail_page(request, slug):
-    print("DJANGO SAYS", request.method, request.path, request.user)
-    # obj = BlogPost.objects.get(slug=slug)
-    # queryset = BlogPost.objects.filter(slug=slug)
-    # if queryset.count() == 0:
-    #     raise Http404       
-    # obj = queryset.first()
-    obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
-    context = {"object": obj} # {"title": obj.title}
-    return render(request, template_name, context)
-
-
 # CRUD
 
 # GET -> Retrieve / List
--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path # url
 from blog.views import (
-    blog_post_detail_page,
+    blog_post_create_view,
+    blog_post_detail_view,
     blog_post_list_view,
+    blog_post_update_view,
+    blog_post_delete_view,
 )
 from .views import (
     home_page,
@@ -30,8 +33,14 @@ from .views import (
 urlpatterns = [
     path('', home_page),
     path('blog/', blog_post_list_view),
-    path('blog/<str:slug>/', blog_post_detail_page),
-    # re_path(r'^blog/(?P<slug>\w+)/$', blog_post_detail_page),
+    path('blog/<str:slug>/', blog_post_detail_view),
+    path('blog/<str:slug>/edit/', blog_post_update_view),
+    path('blog/<str:slug>/delete/', blog_post_delete_view),
+
+    path('blog-new/', blog_post_create_view),
+
+
+    # re_path(r'^blog/(?P<slug>\w+)/$', blog_post_detail_view),
     path('page/', about_page),
     path('pages/', about_page),
     re_path(r'^pages?/$', about_page),
