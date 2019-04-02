8c584df2d6822628653e068d3996c86e76e0d015
CFE <hello@teamcfe.com>
2019-04-02 16:05:18 -0700

    27 - CRUD and Views

--- src/blog/views.py
+++ src/blog/views.py
@@ -9,6 +9,8 @@ from .models import BlogPost
 # filter -> [] objects
 
 def blog_post_detail_page(request, slug):
+    print("DJANGO SAYS", request.method, request.path, request.user)
+    # obj = BlogPost.objects.get(slug=slug)
     # queryset = BlogPost.objects.filter(slug=slug)
     # if queryset.count() == 0:
     #     raise Http404       
@@ -17,3 +19,23 @@ def blog_post_detail_page(request, slug):
     template_name = 'blog_post_detail.html'
     context = {"object": obj} # {"title": obj.title}
     return render(request, template_name, context)
+
+
+# CRUD
+
+# GET -> Retrieve / List
+
+# POST -> Create / Update / DELETE
+
+# Create Retrieve Update Delete
+
+
+
+
+
+
+
+
+
+
+
