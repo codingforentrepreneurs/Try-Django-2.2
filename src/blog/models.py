d66968d653a3e648ba4990a420ad0465ccdb8980
CFE <hello@teamcfe.com>
2019-04-02 14:38:13 -0700

    26 - A Unique Slug

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class BlogPost(models.Model):
     # id = models.IntegerField() # pk
     title  = models.TextField()
-    slug    = models.SlugField() # hello world -> hello-world
+    slug   = models.SlugField(unique=True) # hello world -> hello-world
     content  = models.TextField(null=True, blank=True)
 
--- src/blog/views.py
+++ src/blog/views.py
@@ -9,11 +9,11 @@ from .models import BlogPost
 # filter -> [] objects
 
 def blog_post_detail_page(request, slug):
-    queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() >= 1:
-        obj = queryset.first()
-    
-    # obj = get_object_or_404(BlogPost, slug=slug)
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() == 0:
+    #     raise Http404       
+    # obj = queryset.first()
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {"object": obj} # {"title": obj.title}
     return render(request, template_name, context)
