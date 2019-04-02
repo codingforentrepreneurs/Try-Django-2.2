a0d7125e4b943b4f8d91d7eb7cc5dfefb20e9a22
CFE <hello@teamcfe.com>
2019-04-02 14:33:12 -0700

    25 - QuerySet Lookups

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,10 +5,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 
+# GET -> 1 object
+# filter -> [] objects
 
 def blog_post_detail_page(request, slug):
-    # obj = BlogPost.objects.get(slug=slug)
-    obj = get_object_or_404(BlogPost, slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() >= 1:
+        obj = queryset.first()
+    
+    # obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {"object": obj} # {"title": obj.title}
     return render(request, template_name, context)
