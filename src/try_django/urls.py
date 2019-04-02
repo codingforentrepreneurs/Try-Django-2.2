fc9d8317975a3e7d81024b6ff6fa732a7eb4cd51
CFE <hello@teamcfe.com>
2019-04-02 10:41:00 -0700

    6 - A First URL Mapping

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import home_page
+
 urlpatterns = [
+    path('', home_page),
     path('admin/', admin.site.urls),
 ]
