b62dcde16ead85abf1459302bf9e59c779522e98
CFE <hello@teamcfe.com>
2019-04-02 10:44:14 -0700

    7 - Multiple Views

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import (
+    home_page,
+    about_page,
+    contact_page
+)
+
 
 urlpatterns = [
     path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
--- src/try_django/views.py
+++ src/try_django/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def home_page(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def about_page(request):
+    return HttpResponse("<h1>About Us</h1>")
+
+
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Us</h1>")
