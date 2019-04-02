be3015046ae832eb435de6477887af7958816be4
CFE <hello@teamcfe.com>
2019-04-02 11:56:38 -0700

    16 - Built In Template Tags

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -5,7 +5,9 @@ from django.template.loader import get_template
 
 def home_page(request):
     my_title = "Hello there...."
-    context = {"title": my_title}
+    context = {"title": "my title"}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
     return render(request, "home.html", context)
 
 
