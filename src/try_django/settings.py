958e1b2e8419fdb4fe804905b24a9fd3446703d8
CFE <hello@teamcfe.com>
2019-04-02 10:58:47 -0700

    10 - Loading a HTML Template

--- src/try_django/settings.py
+++ src/try_django/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'try_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
