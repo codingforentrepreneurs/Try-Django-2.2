c3c9e1d11026ede663a0b8cb8a228b2c8163f891
CFE <hello@teamcfe.com>
2019-04-02 16:10:51 -0700

    28 - CRUD View Outline

--- src/blog/views.py
+++ src/blog/views.py
@@ -31,6 +31,44 @@ def blog_post_detail_page(request, slug):
 
 
 
+def blog_post_list_view(request):
+    # list out objects 
+    # could be search
+    template_name = 'blog_post_list.html'
+    context = {'object_list': []}
+    return render(request, template_name, context) 
+
+
+
+def blog_post_create_view(request):
+    # create objects
+    # ? use a form
+    template_name = 'blog_post_create.html'
+    context = {'form': None}
+    return render(request, template_name, context)  
+
+
+def blog_post_detail_view(request, slug):
+    # 1 object -> detail view
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_detail.html'
+    context = {"object": obj}
+    return render(request, template_name, context)   
+
+
+def blog_post_update_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_update.html'
+    context = {"object": obj, 'form': None}
+    return render(request, template_name, context)  
+
+
+def blog_post_delete_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_delete.html'
+    context = {"object": obj}
+    return render(request, template_name, context)  
+
 
 
 
