9f17efd6a246f986435b6fe59e528f6e48fef7fd
CFE <hello@teamcfe.com>
2019-04-03 13:24:27 -0700

    48 - Publish Date, Timestamp & Updated

--- src/blog/models.py
+++ src/blog/models.py
@@ -11,13 +11,19 @@ class BlogPost(models.Model): # blogpost_set -> queryset
     title  = models.CharField(max_length=120)
     slug   = models.SlugField(unique=True) # hello world -> hello-world
     content  = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_date', '-updated', '-timestamp']
 
     def get_absolute_url(self):
         return f"/blog/{self.slug}"
 
     def get_edit_url(self):
-        return f"{self.get_absolute_url}/edit"
+        return f"{self.get_absolute_url()}/edit"
 
     def get_delete_url(self):
-        return f"{self.get_absolute_url}/delete"
+        return f"{self.get_absolute_url()}/delete"
 
