f633fb02000e4b70205b5f8a2f0ef2dc947699de
CFE <hello@teamcfe.com>
2019-04-03 13:36:54 -0700

    50 - Published and Draft Posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -21,6 +21,9 @@ def blog_post_list_view(request):
     # list out objects 
     # could be search
     qs = BlogPost.objects.all().published() # queryset -> list of python object
+    if request.user.is_authenticated:
+        my_qs = BlogPost.objects.filter(user=request.user)
+        qs = (qs | my_qs).distinct()
     template_name = 'blog/list.html'
     context = {'object_list': qs}
     return render(request, template_name, context) 
