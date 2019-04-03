bdb4b392516d2ff7c013c27a17da9a5954bde932
CFE <hello@teamcfe.com>
2019-04-03 10:37:11 -0700

    39 - Associate Blog Post to a User with Foreign Keys

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
-class BlogPost(models.Model):
+User = settings.AUTH_USER_MODEL
+
+class BlogPost(models.Model): # blogpost_set -> queryset
     # id = models.IntegerField() # pk
+    user    = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title  = models.CharField(max_length=120)
     slug   = models.SlugField(unique=True) # hello world -> hello-world
     content  = models.TextField(null=True, blank=True)
