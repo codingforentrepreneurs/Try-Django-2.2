e7e9c05348bea84518fd7d73f6fa920fc255a14d
CFE <hello@teamcfe.com>
2019-04-03 10:19:39 -0700

    37 - Validate Data on Fields

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -13,3 +13,10 @@ class BlogPostModelForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'slug', 'content']
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        qs = BlogPost.objects.filter(title__iexact=title)
+        if qs.exists():
+            raise forms.ValidationError("This title has already been used. Please try again.")
+        return title
--- src/try_django/forms.py
+++ src/try_django/forms.py
@@ -5,3 +5,11 @@ class ContactForm(forms.Form):
     full_name = forms.CharField()
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
+
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        print(email)
+        if email.endswith(".edu"):
+            raise forms.ValidationError("This is not a valid email. Please don't use .edu.")
+        return email
