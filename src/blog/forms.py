8db9a124bdfa17b1a4815917e7c2a3c14334f8e4
CFE <hello@teamcfe.com>
2019-04-03 10:57:48 -0700

    42 - Better Validation on Update Views

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -15,8 +15,11 @@ class BlogPostModelForm(forms.ModelForm):
         fields = ['title', 'slug', 'content']
 
     def clean_title(self, *args, **kwargs):
+        instance = self.instance
         title = self.cleaned_data.get('title')
         qs = BlogPost.objects.filter(title__iexact=title)
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk) # id=instance.id
         if qs.exists():
             raise forms.ValidationError("This title has already been used. Please try again.")
         return title
