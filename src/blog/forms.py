4a71cea7e62e99341ce59c0496fec0a1e660d5f6
John R Perry <john@j-26.com>
2019-09-01 18:03:43 -0600

    Deleted unused class

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -2,12 +2,6 @@ from django import forms
 
 from .models import BlogPost 
 
-class BlogPostForm(forms.Form):
-    title = forms.CharField()
-    slug = forms.SlugField()
-    content = forms.CharField(widget=forms.Textarea)
-
-
 
 class BlogPostModelForm(forms.ModelForm):
     class Meta:
